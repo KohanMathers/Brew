50ec5501276ba82d7dd323460bb5bff91060c54d
Kohan Mathers <kmathers@kmathers.co.uk>
2025-05-01 00:59:22 +0100

    Add CustomError class to use correct error names (e.g. ParseError instead of Error)

--- frontend/errors.ts
+++ frontend/errors.ts
@@ -1,8 +1,15 @@
-export class ParseError extends Error {
+class CustomError extends Error {
     constructor(message: string) {
         super(message);
         if (Error.captureStackTrace) {
-            Error.captureStackTrace(this, ParseError);
+            Error.captureStackTrace(this, CustomError);
         }
+        this.name = this.constructor.name;
+    }
+}
+
+export class ParseError extends CustomError {
+    constructor(message: string) {
+        super(message);
     }
 }
