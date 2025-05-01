ff743e748050018a8f6d311fc46e065d9baabf25
Kohan Mathers <kmathers@kmathers.co.uk>
2025-05-01 01:37:43 +0100

    Add InterpretError as default case

--- runtime/interpreter.ts
+++ runtime/interpreter.ts
@@ -1,12 +1,17 @@
 import { ValueType, RuntimeValue } from "./values.ts"
 import { NodeType, Stmt } from "../frontend/ast.ts"
+import { InterpretError } from "../frontend/errors.ts";
 
 export function Evaluate (astNode:Stmt): RuntimeValue {
     switch (astNode.kind) {
         case "NumericLiteral":
             return { value: ((astNode as NumericLiteral.value)), type: "number" } as NumberValue;
+        case "NullLiteral":
+            return { value: "null", type: "null" } as NullValue;
 
         default:
-            return { value: "null", type: "null" } as NullValue;
+            throw new InterpretError(
+                `The following AST node has not yet been setup for interpretation: ${astNode.kind}`
+            );
     }    
 }
