3c861d53cfe98264e81cacae6ba82a1a7d812337
Kohan Mathers <kmathers@kmathers.co.uk>
2025-05-02 01:47:25 +0100

    Finish environment.ts

--- runtime/environment.ts
+++ runtime/environment.ts
@@ -11,16 +11,30 @@ export default class Environment {
 
     public declareVariable(varname: string, value: RuntimeValue): RuntimeValue {
         if (this.variables.has(varname)) {
-            throw `Cannot declare variable ${varname}: Already declared`;
+            throw `Cannot declare variable ${varname}: Already declared.`;
         }
 
         this.variables.set(varname, value);
         return value;
     }
 
-    public assignVariable(varname: string, value: RuntimeValue): RuntimeValue {}
+    public assignVariable(varname: string, value: RuntimeValue): RuntimeValue {
+        const env = this.resolve(varname);
+        env.variables.set(varname, value) as RuntimeValue;
+        return value;
+    }
+
+    public lookupVariable(varname: string): RuntimeValue {
+        const env = this.resolve(varname);
+        return env.variables.get(varname);
+    }
 
     public resolve(varname: string): Environment {
         if (this.variables.has(varname)) return this;
+
+        if (this.parent == undefined)
+            throw `Cannot resolve '${varname}': Does not exist.`;
+
+        return this.parent.resolve(varname);
     }
 }
